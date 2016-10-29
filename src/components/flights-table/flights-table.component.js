77c621524e2433f98eae2859373f5af29c590944
Piotr Sztymelski <sztymel@gmail.com>
2016-10-29 15:36:55 +0200

    sortBy init

--- src/components/flights-table/flights-table.component.js
+++ src/components/flights-table/flights-table.component.js
@@ -1,8 +1,6 @@
 import template from './flights-table-component.html';
 import controller from './flights-table.controller.js';
 
-controller.$inject = ['dispatcher'];
-
 export const FlightsTableComponent = {
     bindings: {
         flights: '<'
--- src/components/flights-table/flights-table.controller.js
+++ src/components/flights-table/flights-table.controller.js
@@ -1,14 +1,40 @@
 import moment from 'moment';
 
-export default function FlightsTableCtrl (dispatcher) {
+export default function FlightsTableCtrl () {
     const ctrl = this;
     ctrl.computeTravelTime = computeTravelTime;
     ctrl.tableHeaders = ['Departure', 'Arrival', 'Price', 'Travel time'];
     ctrl.formatDate = formatDate;
+    ctrl.addFavourite = addFavourite;
+    ctrl.sortBy = sortBy;
+    let descending = false;
 
+
+    function sortBy(header) {
+        switch (header) {
+            case 'Departure':
+                ctrl.flights.sort(byDate);
+
+                break;
+            default:
+                break;
+        }
+
+        function byDate(a, b) {
+            console.log(a);
+            console.log(b);
+            descending = !descending;
+            if (descending) return new Date(a.dateFrom) < new Date(b.dateFrom);
+            else return new Date(a.dateFrom) > new Date(b.dateFrom);
+        }
+    }
 
     function formatDate(date) {
         return moment(date).format('LLL');
+    }
+
+    function addFavourite(flight) {
+        flight.favourite = !flight.favourite;
     }
 
     function computeTravelTime(flight) {
